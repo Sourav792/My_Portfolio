2c7fe9cd29f16ee5e6ee0ffe5e80222bfd986ef5
sourav singh <sourav859987@gmail.com>
2024-02-05 10:55:25 +0530

    add hamburger link state

--- script.js
+++ script.js
@@ -4,17 +4,3 @@ hamburgerIcon.onclick = () => {
     let navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active')
 }
-let hamburgerLink = () => {
-    let navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle("active");
-}
-
-let aboutSection = document.querySelector('#about-link');
-let serviceSection = document.querySelector('#service-link');
-let projectSection = document.querySelector('#project-link');
-let contactSection = document.querySelector('#contact-link');
-
-aboutSection.onclick = hamburgerLink
-serviceSection.onclick = hamburgerLink
-projectSection.onclick = hamburgerLink
-contactSection.onclick = hamburgerLink
