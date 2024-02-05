856cdea4fe378ba6c1284726a59f7f7973b863f7
sourav singh <sourav859987@gmail.com>
2024-02-05 10:29:51 +0530

    add hamburger icon

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const hamburgerIcon = document.querySelector('#hamburger-icon');
 
 hamburgerIcon.onclick = () => {
-    const navLinks = document.querySelector('.nav-links');
+    let navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active')
 }
+let hamburgerLink = () => {
+    let navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle("active");
+}
+
+let aboutSection = document.querySelector('#about-link');
+let serviceSection = document.querySelector('#service-link');
+let projectSection = document.querySelector('#project-link');
+let contactSection = document.querySelector('#contact-link');
+
+aboutSection.onclick = hamburgerLink
+serviceSection.onclick = hamburgerLink
+projectSection.onclick = hamburgerLink
+contactSection.onclick = hamburgerLink
