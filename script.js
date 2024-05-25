2116bb6f56e9f59b9af432b58884fae4da920538
Sourav Singh <sourav859987@gmail.com>
2024-05-25 10:21:03 +0530

    change information

--- script.js
+++ script.js
@@ -2,5 +2,11 @@ const hamburgerIcon = document.querySelector('#hamburger-icon');
 
 hamburgerIcon.onclick = () => {
     let navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active')
+    // navLinks.classList.toggle('active')
 }
+// dark to light mode switcher
+// let mode = document.querySelector('#mode');
+// let modeIcon = document.querySelector('#mode_icon');
+// mode.addEventListener('click',() => {
+
+// })
